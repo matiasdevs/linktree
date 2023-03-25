3dd5ea97673a686d6b46ab2f73a089291198aac8
Matias Amariya <me.em@outlook.com>
2023-03-25 19:44:20 -0300

    added javascript theme toggle

---
+++ main.js
@@ -0,0 +1,11 @@
+function toggleMode(){
+  const html = document.documentElement
+  const img = document.querySelector('#profile > img')
+
+    html.classList.toggle('light')
+    if(html.classList.contains('light')){
+      img.setAttribute('src', 'assets/profile.svg')
+    } else {
+      img.setAttribute('src', 'assets/profile-black.svg')
+    }
+}
